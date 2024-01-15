7aeea51c623dd35f76502afaec1b7eb1e159760a
Wilbert Belgica <156556139+Wilbert-Belgica@users.noreply.github.com>
2024-01-15 15:15:11 +0800

    Delete pw.py

--- pw.py
+++
@@ -1,7 +0,0 @@
-## EASY PW GENERATOR
-# coded by Wilbert Belgica
-print("Welcome to EASY PASSWORD GENERATOR!")
-middle_name = input("What is your Middle Name?\n")
-birthday = input("When is your Birthday(mmddyyyy)?\n")
-color = input("What is your favorite color?\n")
-print("Your password could be:\n"+ middle_name + birthday + color)
